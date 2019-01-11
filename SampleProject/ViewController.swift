d19a02f6f32d2a4056633448fa463330df465e68
Anilkumar <anilkumar@anilkumars-imac-3.local>
2019-01-11 19:22:38 +0530

    Initial Commit

---
+++ SampleProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleProject
+//
+//  Created by Anilkumar on 11/01/19.
+//  Copyright © 2019 Anilkumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
